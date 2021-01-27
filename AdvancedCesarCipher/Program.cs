d15a102dbd36fd297e713286eabb35c4dd7f21a8
NONAME1103 <faparicio1103@gmail.com>
2021-01-27 17:30:59 +0100

    encrypt1

--- AdvancedCesarCipher/Program.cs
+++ AdvancedCesarCipher/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Channels;
 
 namespace AdvancedCesarCipher {
     
@@ -15,12 +17,20 @@ namespace AdvancedCesarCipher {
             Using this and a three letter key run some tests to work out which is the
             more secure when it comes to a brute force attack */
             
-            // GetKey
-            char[] k = GetKey();
-            // GetMessage
+            // (ASCII codes lowercase: 97-122)
+            
+            // Gets key as list of integer numbers to shift cipher by
+            int[] k = GetKey();
+            // Gets message as string (of length greater than 0)
+            string m = GetMessage();
+            // Get cipher type
+            int type = GetType();
+            // Encrypt message
+            string secret = type == 1 ? Encrypt1(m, k) : Encrypt2(m, k);
+            Test(m, k, secret);
         }
 
-        public static char[] GetKey() {
+        public static int[] GetKey() {
             string n = "";
             while (true) {
                 Console.Write("Enter the key length (n): ");
@@ -30,24 +40,70 @@ namespace AdvancedCesarCipher {
                     if (0 < intInput) {
                         break;
                     }
-                    Console.WriteLine("\n\nInvalid Input!");
+                    Console.WriteLine("\nInvalid Input!");
                 }
                 catch (FormatException) {
-                    Console.WriteLine("\n\nInvalid Input!");
+                    Console.WriteLine("\nInvalid Input!");
                 }
             }
             return MakeKey(Convert.ToInt32(n));
         }
 
-        public static char[] MakeKey(int n) {
-            char[] key = new char[n];
+        public static int[] MakeKey(int n) {
+            int[] key = new int[n];
             Random rand = new Random();
             for (int i = 0; i < n; i++) {
-                // Chooses a random lowercase letter
-                int num = rand.Next(1, 26);
+                int num = rand.Next(1, 27);
                 key[i] = num;
             }
             return key;
+        }
+
+        public static string GetMessage() {
+            while (true) {
+                Console.Write("Enter the message to encrypt: ");
+                string message = Console.ReadLine();
+                if (message.Length > 0) {
+                    return message;
+                }
+                Console.WriteLine("\nInvalid Input!");
+            }
+        }
+
+        public static int GetType() {
+            Console.WriteLine (@"
+    Please select a cipher option:
+    1) Reset shift each time
+    2) Carry on from last shift each time
+    ---------------------------------------");
+            Console.Write ("    Option: ");
+            string type = Console.ReadLine();
+            if (Array.IndexOf(new string[] {"1", "2"}, type) != -1) {
+                return Convert.ToInt32(type);
+            }
+            Console.WriteLine ("\nInvalid Option!");
+            return GetType();
+        }
+
+        public static string Encrypt1(string m, int[] k) {
+            char[] secret = m.ToCharArray();
+            for (int i = 0; i < secret.Length; i++) {
+                char temp = (char) (secret[i] + k[i % 3]);
+                secret[i] = temp > 122 ? (char) (temp - 26) : temp;
+            }
+            return new string (secret);
+        }
+        
+        public static string Encrypt2(string m, int[] k) {
+            string secret = "";
+            return secret;
+        }
+
+        public static void Test(string m, int[] k, string secret) {
+            Console.WriteLine($"\nPlain: {m}");
+            Console.Write("Key: ");
+            k.ToList().ForEach(x => Console.Write($"{Convert.ToString(x)}, "));
+            Console.WriteLine($"\nEncrypted: {secret}");
         }
     }
 }
